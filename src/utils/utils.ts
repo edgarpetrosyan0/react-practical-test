c8a26fcf566ccdecfaac4d017fd94c2828c5d627
Edgar Petrosyan <edgarpetrosyanak@gmail.com>
2025-04-30 02:44:10 +0400

    fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,9 @@
 export const exampleFn = () => {
     
 }
+
+// export function classNames(classObject:any) {
+//     return Object.keys(classObject)
+//       .filter((key) => classObject[key]) 
+//       .join(" ");
+//   }
